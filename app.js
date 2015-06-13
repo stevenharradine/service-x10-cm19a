ff5eb9adad5ff7c31afafc160991ceab7c338dca
steven harradine <steven.harradine@telus.com>
2015-06-13 17:59:15 -0400

    Inital commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var http = require('http'),
+    sh = require('execSync');	// executing system commands
+
+http.createServer(function (req, res) {
+	var urlSplitOnSegment = req.url.split('?')[1];
+
+	if (urlSplitOnSegment != undefined) {
+		var urlSplit = urlSplitOnSegment.split('&');
+
+		if (urlSplit != undefined) {
+			var output     = "",
+			    houseCode  = "",
+			    unitCode   = "",
+			    statusCode = "",
+			    command    = "";
+
+			for (i = 0; i < urlSplit.length; i++) {
+				var name  = urlSplit[i].split('=')[0],
+				    value = urlSplit[i].split('=')[1];
+
+				if (name == "houseCode") {
+					houseCode = value;
+				} else if (name == "unitCode") {
+					unitCode = value;
+				} else if (name == "statusCode") {
+					statusCode = value;
+				}
+			}
+
+			command = "echo " + statusCode + houseCode + unitCode + " > /home/pi/pycm19a/cm19a/in";
+
+			sh.exec (command);
+			output += command;
+
+			res.writeHead ("200", {'Content-Type': 'text/plain'});
+
+			console.log (output);
+			res.end (output);
+		} else {
+			res.writeHead ("500", {'Content-Type': 'text/plain'});
+			res.end ("Error");
+		}
+	} else {
+		var output = "no params passes";
+		res.writeHead ("200", {'Content-Type': 'text/plain'});
+
+		console.log (output);
+		res.end (output);
+	}
+}).listen(666);
